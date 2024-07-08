07d7abbd98b7f19d9d7c836cc6b69d135afdacbf
Michael Ho <MHova@users.noreply.github.com>
2024-07-08 10:46:27 -0700

    Refactor: rename variable

--- src/test/java/com/mhova/domain/GameTest.java
+++ src/test/java/com/mhova/domain/GameTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-	private Game game;
+	private Game classUnderTest;
 	private Board board;
 	private Player alice;
 	private Player bob;
@@ -20,17 +20,17 @@ class GameTest {
 		board = new Board();
 		alice = new Player("Alice");
 		bob = new Player("Bob");
-		game = new Game(alice, Optional.of(bob), board);
+		classUnderTest = new Game(alice, Optional.of(bob), board);
 	}
 
 	@Test
 	void newGameWithTwoPlayers() {
-		final List<Players> active = game.getActive();
+		final List<Players> active = classUnderTest.getActive();
 		assertEquals(2, active.size());
 		assertEquals(Players.PLAYER_1, active.get(0));
 		assertEquals(Players.PLAYER_2, active.get(1));
 
-		final List<Players> priority = game.getPriority();
+		final List<Players> priority = classUnderTest.getPriority();
 		assertEquals(2, priority.size());
 		assertEquals(Players.PLAYER_1, priority.get(0));
 		assertEquals(Players.PLAYER_2, priority.get(1));
@@ -38,12 +38,12 @@ class GameTest {
 
 	@Test
 	void newGameWithOnePlayer() {
-		game = new Game(alice, Optional.empty(), board);
-		final List<Players> active = game.getActive();
+		classUnderTest = new Game(alice, Optional.empty(), board);
+		final List<Players> active = classUnderTest.getActive();
 		assertEquals(1, active.size());
 		assertEquals(Players.PLAYER_1, active.get(0));
 
-		final List<Players> priority = game.getPriority();
+		final List<Players> priority = classUnderTest.getPriority();
 		assertEquals(1, priority.size());
 		assertEquals(Players.PLAYER_1, priority.get(0));
 	}
@@ -52,106 +52,106 @@ class GameTest {
 	void advanceStep() {
 		// Player 1's turn
 		assertPlayer1HasPriority();
-		Pair<Players, Step> retVal = game.advanceStep();
+		Pair<Players, Step> retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.UPKEEP), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.DRAW), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.MAIN_1), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.BEGINNING_OF_COMBAT), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.ATTACKERS), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.BLOCKERS), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.FIRST_STRIKE_DAMAGE), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.DAMAGE), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.END_OF_COMBAT), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.MAIN_2), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.END), retVal);
 		assertPlayer1HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.CLEANUP), retVal);
 		assertPlayer1HasPriority();
 		
 		// Player 2's turn
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.UNTAP), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.UPKEEP), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.DRAW), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.MAIN_1), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.BEGINNING_OF_COMBAT), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.ATTACKERS), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.BLOCKERS), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.FIRST_STRIKE_DAMAGE), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.DAMAGE), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.END_OF_COMBAT), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.MAIN_2), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.END), retVal);
 		assertPlayer2HasPriority();
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_2, Step.CLEANUP), retVal);
 		assertPlayer2HasPriority();
 		
 		// Back to Player 1
-		retVal = game.advanceStep();
+		retVal = classUnderTest.advanceStep();
 		assertEquals(Pair.with(Players.PLAYER_1, Step.UNTAP), retVal);
 		assertPlayer1HasPriority();
 	}
 	
 	@Test
 	void passPriority() {
-		Optional<Players> retVal = game.passPriority();
+		Optional<Players> retVal = classUnderTest.passPriority();
 		assertEquals(Optional.of(Players.PLAYER_2), retVal);
-		retVal = game.passPriority();
+		retVal = classUnderTest.passPriority();
 		assertEquals(Optional.empty(), retVal);
 	}
 	
 	void assertPlayer1HasPriority() {
-		final List<Players> priority = game.getPriority();
+		final List<Players> priority = classUnderTest.getPriority();
 		assertEquals(2, priority.size());
 		assertEquals(Players.PLAYER_1, priority.get(0));
 	}
 	
 	void assertPlayer2HasPriority() {
-		final List<Players> priority = game.getPriority();
+		final List<Players> priority = classUnderTest.getPriority();
 		assertEquals(2, priority.size());
 		assertEquals(Players.PLAYER_2, priority.get(0));
 	}

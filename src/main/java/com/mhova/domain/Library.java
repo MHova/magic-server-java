6839a94e03aa32fc4decd904a264726141c1625e
Michael Ho <MHova@users.noreply.github.com>
2024-07-15 11:18:51 -0700

    Start moving functionality into Library class
    Aaaaaand miraculously ArrayDeque is back in commission today???

---
+++ src/main/java/com/mhova/domain/Library.java
@@ -0,0 +1,33 @@
+package com.mhova.domain;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Library {
+	private final Deque<Card> deque = new ArrayDeque<>();
+
+	public Library(final List<Card> cards) {
+		deque.addAll(cards);
+	}
+
+	public void putCardXFromTop(final int x, final Card card) {
+		final int limit = x <= deque.size() ? x : deque.size() + 1;
+		final LinkedList<Card> temp = new LinkedList<>();
+
+		for (int n = 1; n < limit; n++) {
+			temp.addFirst(deque.poll());
+		}
+
+		deque.addFirst(card);
+		temp.stream().forEachOrdered(c -> deque.addFirst(c));
+	}
+
+	public List<Card> asList() {
+		final List<Card> retVal = new ArrayList<>();
+		retVal.addAll(deque);
+		return retVal;
+	}
+}
---
+++ src/test/java/com/mhova/domain/LibraryTest.java
@@ -0,0 +1,190 @@
+package com.mhova.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class LibraryTest {
+	private Library classUnderTest;
+
+	@Test
+	void addToTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(two);
+		cards.add(three);
+		cards.add(four);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(1, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void addSecondFromTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(one);
+		cards.add(three);
+		cards.add(four);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(2, two);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void addThirdFromTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(one);
+		cards.add(two);
+		cards.add(four);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(3, three);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void addFourthFromTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(one);
+		cards.add(two);
+		cards.add(three);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(4, four);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void addFifthFromTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(one);
+		cards.add(two);
+		cards.add(three);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(5, four);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void treatZeroXAsAddToTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(two);
+		cards.add(three);
+		cards.add(four);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(-1, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void treatNegativeXAsAddToTop() {
+		final Card one = new Card("1");
+		final Card two = new Card("2");
+		final Card three = new Card("3");
+		final Card four = new Card("4");
+
+		final List<Card> cards = new LinkedList<>();
+		cards.add(two);
+		cards.add(three);
+		cards.add(four);
+		classUnderTest = new Library(cards);
+		classUnderTest.putCardXFromTop(-1, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(4, result.size());
+		assertEquals(one, result.get(0));
+		assertEquals(two, result.get(1));
+		assertEquals(three, result.get(2));
+		assertEquals(four, result.get(3));
+	}
+
+	@Test
+	void addNegativeXToEmptyLibrary() {
+		final Card one = new Card("1");
+		classUnderTest = new Library(Collections.emptyList());
+		classUnderTest.putCardXFromTop(-1, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(1, result.size());
+		assertEquals(one, result.get(0));
+	}
+
+	@Test
+	void addZeroToEmptyLibrary() {
+		final Card one = new Card("1");
+		classUnderTest = new Library(Collections.emptyList());
+		classUnderTest.putCardXFromTop(0, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(1, result.size());
+		assertEquals(one, result.get(0));
+	}
+
+	@Test
+	void addToTopToEmptyLibrary() {
+		final Card one = new Card("1");
+		classUnderTest = new Library(Collections.emptyList());
+		classUnderTest.putCardXFromTop(1, one);
+		final List<Card> result = classUnderTest.asList();
+		assertEquals(1, result.size());
+		assertEquals(one, result.get(0));
+	}
+}

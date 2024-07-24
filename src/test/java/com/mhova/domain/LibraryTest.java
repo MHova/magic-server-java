cdf85ce3635fe1bbff0a217c7cbeb1a05665ced4
Michael Ho <MHova@users.noreply.github.com>
2024-07-24 11:12:42 -0700

    Add MongoDB driver dependency
    And also fix a wrong test import

--- src/test/java/com/mhova/domain/LibraryTest.java
+++ src/test/java/com/mhova/domain/LibraryTest.java
@@ -1,7 +1,7 @@
 package com.mhova.domain;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.LinkedList;
